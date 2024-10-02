392c857502642091458aab3c3760409872d8b64f
jaketeneyck <jaketeneyck3@gmail.com>
2024-10-02 13:59:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def main():
 
 
     while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    
         screen.fill((0,0,0))
         pygame.display.flip()   
 
