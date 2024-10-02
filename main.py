dd022d40e452cab59b7f9eb2e198028bdd470da9
jaketeneyck <jaketeneyck3@gmail.com>
2024-10-02 14:36:09 -0400

    Created player class and have the game screen displaying the player's sprite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     # Initialize pygame and set the game window size
@@ -11,14 +12,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    # Create player object
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     # Main game loop
     while True:
-
+        # Check that the player hasn't quit the game, quit if yes
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
+
+        # Re-render the screen
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()   
 
         dt = clock.tick(60) / 1000
