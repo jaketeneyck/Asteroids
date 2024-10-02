44445a9cfb8fbd78750555c104ab165655f2461b
jaketeneyck <jaketeneyck3@gmail.com>
2024-10-02 14:08:33 -0400

    Added clock object and delta time to track game speed

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ def main():
     print("Starting asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Create clock to limit FPS
+    clock = pygame.time.Clock()
+    dt = 0
 
+    # Main game loop
     while True:
 
         for event in pygame.event.get():
@@ -16,6 +20,8 @@ def main():
     
         screen.fill((0,0,0))
         pygame.display.flip()   
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
